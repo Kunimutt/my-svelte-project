21a17f4b882b11cb98b376353f8ccdb0e6645508
Moa Haldorson <moa.haldorson@knowit.se>
2024-07-19 17:56:26 +0200

    entire app setup

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Todos from './lib/Todos.svelte';
+    import Alert from './lib/Alert.svelte';
+
+    import { todos } from './stores.js';
+</script>
+
+<Alert />
+<Todos bind:todos={$todos} />
---
+++ src/lib/TodosStatus.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { TodoType } from '../types/todo.type';
+    export let todos: TodoType[];
+
+    $: totalTodos = todos.length;
+    $: completedTodos = todos.filter((todo: TodoType) => todo.completed).length;
+
+    let headingEl: HTMLElement;
+
+    export function focus() {
+        // shorter version: export const focus = () => headingEl.focus()
+        headingEl.focus();
+    }
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<h2 id="list-heading" bind:this={headingEl} tabindex={-1}>
+    {completedTodos} out of {totalTodos} items completed
+</h2>
